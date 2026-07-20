8d9b5276cb006647340eba0dd26c99c72f43f503
simonsaavedra94-cyber <simonsaavedra94@gmail.com>
2026-07-20 10:17:24 +0200

    Add files via upload

--- src/ProcesadorAgenda.java
+++ src/ProcesadorAgenda.java
@@ -10,14 +10,13 @@ public class ProcesadorAgenda {
 
         matrizSala1[8][1] = "ReunioJava";
         matrizSala1[9][1] = "ReunioJava";
-        matrizSala1[14][5] = "Tancat";
+        matrizSala1[14][5] = "Cerrado";
 
         matrizSala2[10][12] = "ReunioC";
-        matrizSala2[14][5] = "Tancat";
+        matrizSala2[14][5] = "Cerrado";
 
         mapaSalas.put("Sala1", matrizSala1);
         mapaSalas.put("Sala2", matrizSala2);
-
 
         System.out.println("incidencias.log generado en silencio.");
 
